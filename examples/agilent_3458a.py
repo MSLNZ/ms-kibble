24f4e4bd05aa96de96fb69b22fa77eb2d551fb57
jborbely <joe.borbely@gmail.com>
2024-06-11 11:10:39 +1200

    create example scripts

---
+++ examples/agilent_3458a.py
@@ -0,0 +1,13 @@
+from equipment_register import records
+from kibble import Agilent3458A
+
+dmm = Agilent3458A(records['3458a'])
+
+dt = dmm.configure(aperature=0.1)
+dmm.initiate()
+
+data = dmm.fetch()
+print(f'approx. time between samples: {dt:.3f}')
+print(f'samples: {data}')
+
+dmm.disconnect()
---
+++ examples/spincore_pulseblaster.py
@@ -0,0 +1,15 @@
+from equipment_register import records
+from kibble import PulseBlaster
+
+blaster = PulseBlaster(records['blaster'])
+
+blaster.configure_two_pulses(width=10e-6, delay=5e-6)
+
+while True:
+    blaster.trigger()
+    x = input('Press ENTER to send trigger, -1 to exit: ')
+    if x == '-1':
+        break
+
+blaster.stop()
+blaster.disconnect()
