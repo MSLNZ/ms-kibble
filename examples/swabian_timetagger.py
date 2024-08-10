15673f9281e8e0f2a4ace82dcd21733a8e0dff33
jborbely <joe.borbely@gmail.com>
2024-08-10 18:54:42 +1200

    create a time-tag Channel in the example

--- examples/swabian_timetagger.py
+++ examples/swabian_timetagger.py
@@ -2,11 +2,15 @@
 
 from kibble import TriggeredTIA
 
-with TriggeredTIA(trigger=1, start=2, stop=3, duration=1) as tia:
+# We can specify the trigger, start and stop channels as integers, or, to have
+# more control over the configuraton of each channel, as a `Channel` object
+start = TriggeredTIA.create_channel(2, level=0.6)
+
+with TriggeredTIA(trigger=1, start=start, stop=3, duration=1) as tia:
     # Start the time-interval analyser measurement, this is not a blocking call and this script will continue
     tia.start()
 
-    # Implement code to send a trigger pulse to Channel 1.
+    # You could implement code to send a trigger pulse to Channel 1 here.
     # Ideally, there would also be start-stop signals connected to Channels 2 and 3.
 
     # Calling tia.intervals() will block until the measurement is done.
