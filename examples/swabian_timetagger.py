b41168bbb6857738906473e025e4a15c48ca9bbb
jborbely <joe.borbely@gmail.com>
2024-08-12 14:59:28 +1200

    docs: fix typo

--- examples/swabian_timetagger.py
+++ examples/swabian_timetagger.py
@@ -3,7 +3,7 @@
 from kibble import TimeIntervalAnalyser
 
 # We can specify the start and stop channels as integers, or, to have
-# more control over the configuraton of each channel, as a `Channel` object
+# more control over the configuration of each channel, as a `Channel` object
 start = TimeIntervalAnalyser.create_channel(2, deadtime=2000, delay=100, frequency=2.96e6, level=0.6)
 
 # If you want to synchronise the measurement with other equipment, the TimeIntervalAnalyser
