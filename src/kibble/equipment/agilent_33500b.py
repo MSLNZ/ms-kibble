75b6a1bd2fd8b1af3c78dcbcaf809e890dddda97
fungmsl <yinhsien.fung@callaghaninnovation.govt.nz>
2024-10-16 11:08:28 +1300

    Implementing arbitrary waveform with input data

--- src/kibble/equipment/agilent_33500b.py
+++ src/kibble/equipment/agilent_33500b.py
@@ -85,6 +85,38 @@ class Agilent33500B:
         )
         self._check_error()
 
+    def arb_input(
+        self,
+        channel: int,
+        *,
+        load: float | None = 50,
+    ) -> None:
+        """Configure a SINUSOID waveform for a particular channel.
+
+        Args:
+            channel: The channel number to configure.
+            load: The load termination, in Ohms. In the range 1 to 10 kOhm,
+                or `None` for infinite (High Z).
+        """
+        if channel not in [1, 2]:
+            msg = f"Channel must be either 1 or 2, got {channel}"
+            raise ValueError(msg)
+
+        if load is not None and (load < 1 or load > 10e3):  # noqa: PLR2004
+            msg = f"The terminal load must be between 1 and 10 kOhm or None, got {load}"
+            raise ValueError(msg)
+
+        self._cxn.write(
+            f":OUTPUT{channel}:LOAD {load or 'INFINITY'};"  # OUTPUT must come before SOURCE
+            f":SOURCE{channel}:FUNCTION:ARB:SRATE 10E3;"
+            f":SOURCE{channel}:FUNCTION:ARB:PTPEAK 10;"
+            f":SOURCE{channel}:DATA:ARB dc_ramp, 0.1,0.1,0.1,0.2,0.2,0.2,0.2,0.5;"
+            f":SOURCE{channel}:FUNCTION:ARB dc_ramp;"
+
+
+        )
+        self._check_error()
+
     def clear(self) -> None:
         """Clears the event registers in all register groups and the error queue."""
         self._cxn.write("*CLS")
