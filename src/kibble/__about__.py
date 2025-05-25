623c8f2b93e34235e9df07381023c53a512c47b0
jborbely <joe.borbely@gmail.com>
2025-05-26 10:13:24 +1200

    update copyright year to 2025

--- src/kibble/__about__.py
+++ src/kibble/__about__.py
@@ -5,4 +5,11 @@ from __future__ import annotations
 from ._version import __version__, version_tuple
 
 __author__: str = "Measurement Standards Laboratory of New Zealand"
-__copyright__: str = f"\xa9 2024, {__author__}"
+__copyright__: str = f"\xa9 2024-2025, {__author__}"
+
+__all__: list[str] = [
+    "__author__",
+    "__copyright__",
+    "__version__",
+    "version_tuple",
+]
